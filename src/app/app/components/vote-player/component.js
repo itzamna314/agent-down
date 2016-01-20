c6f6eab09c750a67c565b0cc472a0b05a7b696e0
Kyl Wellman <kyl@gratafy.com>
2016-01-19 21:54:02 -0800

    get observer right

--- src/app/app/components/vote-player/component.js
+++ src/app/app/components/vote-player/component.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	canVote: Ember.computed('accusation.accuser.id', 'me.id', 'me.accusationMade', 'accusation.votes', function(){
+	canVote: Ember.computed('accusation', 'accusation.accuser', 'accusation.accused', 'accusation.votes', 'accusation.votes.@each.player', 'me', 'me.accusationMade', function(){
 		var accuserIsNotMe = this.get('accusation.accuser.id') !== this.get('me.id');
 		var accusedIsNotMe = this.get('accusation.accused.id') !== this.get('me.id');
 		var myVote = this.get('accusation.votes').findBy('player.id', this.get('me.id'));
+        var accusationMade = this.get('me.accusationMade');
 
 		return accuserIsNotMe && accusedIsNotMe && !myVote;
 	}),
