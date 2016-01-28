378802b96d0fc0a3725373ad03e07f655f7e92e5
Kyl Wellman <kyl@gratafy.com>
2016-01-27 23:53:02 -0800

    start adding final reckoning components

---
+++ src/app/app/components/final-reckoning-banner/component.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+        accuserName: Ember.computed('me', 'me.id', 'accuser', 'accuser.id', 
+            function() {
+                if ( this.get('me.id') === this.get('accuser.id') ) {
+                    return 'Your';
+                } else {
+                    return `{accuser.name}'s`;
+                }                    
+            }
+        )
+});
