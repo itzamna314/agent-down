33a80d092d5118641848b55ef3a632059c99d0e8
Kyl Wellman <kyl@gratafy.com>
2016-02-04 00:10:21 -0800

    filter list of joinable games down to those with lat/lon set, and created in the last hour

--- src/server/src/dal/games.go
+++ src/server/src/dal/games.go
@@ -82,7 +82,10 @@ func FindGames(db *sql.DB, state string) ([]*Game, error) {
 		                     FROM game g
 							 JOIN gameStateType gst on gst.id = g.stateId
 		                     JOIN player cr on cr.gameId = g.id
-		                    WHERE g.stateId = ?`,
+		                    WHERE g.stateId = ?
+							  AND g.latitude is not null
+							  AND g.longitude is not null
+						      AND TIMESTAMPDIFF(HOUR, g.createdOn, CURRENT_TIMESTAMP) < 1`,
 		gameStateId[GameState(state)])
 
 	if err != nil {
