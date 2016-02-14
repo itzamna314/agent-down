5ebffd9dff63717dd8eabadbec7d81c0bbc93d06
Kyl Wellman <kyl@gratafy.com>
2016-02-14 15:35:56 -0800

    randomize location order.  Fix css problem

--- src/app/app/components/guess-location-list/component.js
+++ src/app/app/components/guess-location-list/component.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    sortedLocations: Ember.computed('locations', function() {
+        var arr = this.get('locations');
+
+        if ( !arr ) {
+            return [];
+        }
+
+        arr.forEach((item) => {
+            item.set('sortKey', Math.random());
+        });
+
+        return arr.sortBy('sortKey');
+    }),
     actions: {
         commitGuess(location){
             this.sendAction('action', location);
