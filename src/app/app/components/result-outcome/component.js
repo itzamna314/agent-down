9dea1120bb0e1cf34af0898eadd2108ddc116b19
Kyl Wellman <kyl@gratafy.com>
2016-02-28 00:15:14 -0800

    more css

--- src/app/app/components/result-outcome/component.js
+++ src/app/app/components/result-outcome/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    classNames: ['result-outcome'],
     iWin: Ember.computed('me', 'game', 'game.state', function(){
         return this.get('game.state') === 'spyWins' && this.get('me.isSpy') ||
             this.get('game.state') === 'playersWin' && !this.get('me.isSpy');
