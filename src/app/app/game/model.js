e1b228418093525aa7c512e90778b49f34836fe4
Kyl Wellman <kyl@gratafy.com>
2016-05-07 17:24:33 -0700

    drop front-end support for join by email.  Start adding support for join by code

--- src/app/app/game/model.js
+++ src/app/app/game/model.js
@@ -5,6 +5,7 @@ var Game = DS.Model.extend({
     accusations: DS.hasMany('accusation', {async: true}),
     creator: DS.belongsTo('player', {async: true}),
     spy: DS.belongsTo('player', {async: true}),
+    joinCode: DS.attr('string'),
     location: DS.belongsTo('location', {async: true, inverse:'games'}),
     locationGuess: DS.belongsTo('location', {async: true}),
     state: DS.attr('string'),
